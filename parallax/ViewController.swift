5f69cc399078cefa6ba701dd183abcf187a1eff3
Mariah Baysic <mjpb123@gmail.com>
2020-03-30 16:36:24 +0800

    added tableView

--- parallax/ViewController.swift
+++ parallax/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
 
 }
 
